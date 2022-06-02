466d006ef3345ec2f1db55bc81e29a3b5bd232dc
MariaT20 <mariatendo84@gmail.com>
2022-06-02 10:44:57 -0700

    commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * returns: Always 0 (Succes)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a place of art,\n");
+	return (0);
+}
