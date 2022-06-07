668049f81246b550c0dc5ca3b82cb1e08f3e7e12
MariaT20 <mariatendo84@gmail.com>
2022-06-07 10:28:46 -0700

    prints 10 lines of the alphabetic

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * print_alphabet - Print letters multiple times
+ *
+ * Description: Print letters multiple times
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+
+	char alphabet;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar('\n');
+
+		for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+		{
+			_putchar(alphabet);
+		}
+	}
+}
