6d9ea12285d37604343ea8b727e9243cf81884ca
MariaT20 <mariatendo84@gmail.com>
2022-06-07 09:47:12 -0700

    a program that prints _putchar, followed by a new line

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ *
+ */
+int main(void)
+{
+	char Maria[] = "_putchar";
+	int i;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(Maria[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
