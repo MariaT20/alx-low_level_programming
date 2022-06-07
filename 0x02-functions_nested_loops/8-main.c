206566984821dbefca208d608acd30d574126979
MariaT20 <mariatendo84@gmail.com>
2022-06-07 14:42:16 -0700

    prints 24hours

---
+++ 0x02-functions_nested_loops/8-main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+
+
+/**
+ *
+ *  * main - check the code
+ *
+ *   *
+ *
+ *    * Return: Always 0.
+ *
+ */
+int main(void)
+{
+	jack_bauer();
+       	return (0);
+}
+julien@ubuntu:~/0x02$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 _putchar.c 8-main.c 8-24_hours.c -o 8-24
