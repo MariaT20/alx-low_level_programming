4493661b53e79c9dc1c137209ad00529df65dc29
MariaT20 <mariatendo84@gmail.com>
2022-06-08 01:01:16 -0700

    a function that prints all natural numbers from n to 98, followed by a new line

---
+++ 0x02-functions_nested_loops/11-main.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+
+
+/**
+ *  * main - check the code
+ *
+ *   *
+ *
+ *    * Return: Always 0.
+ *
+ */
+int main(void)
+{
+	print_to_98(0);
+	print_to_98(98);
+	print_to_98(111);
+	print_to_98(81);
+	print_to_98(-10);
+	return (0);
+}
+julien@ubuntu:~/0x02$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 _putchar.c 11-main.c 11-print_to_98.c -o 11-98
+
+
