3d0d99913090d5bc1e5c34c28e3e5e1c46d69c9c
MariaT20 <mariatendo84@gmail.com>
2022-06-13 12:39:55 -0700

    a function that prints a string, followed by a new line, to stdout

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+
+
+/**
+ *  swap_int - swaps the values of two integers.
+ *
+ *   @a: value to be evaluate.
+ *
+ *    @b: value to be evaluate.
+ *
+ *     Return: not.
+ */
+void swap_int(int *a, int *b)a
+{
+	int = swap;
+
+	swap = *a;
+	*a = *b;
+	*b = swap;
+}
---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+
+
+/**
+ *  * _puts - prints a string, followed by a new line.
+ *
+ *   * @str: parameter
+ *
+ *    *
+ *
+ *     * Return: void.
+ *
+ */
+void _puts(char *str)
+{
+	puts(str);
+}
