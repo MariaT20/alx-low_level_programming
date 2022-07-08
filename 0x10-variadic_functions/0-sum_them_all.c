31e5d853b529314b87bfd107f1290f0f7f66fe26
MariaT20 <mariatendo84@gmail.com>
2022-07-08 09:51:23 -0700

    a function that prints numbers, followed by a new line

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,12 +1,26 @@
 #include "variadic_functions.h"
 
 /**
- * main - check the code
+ * sum_them_all - returns the sum of all its parameters.
+ * @n: amount of the arguments.
  *
- * Return: Always 0.
+ * Return: sum of its parameters.
  */
-int main(void)
+int sum_them_all(const unsigned int n, ...)
 {
-    print_numbers(", ", 4, 0, 98, -1024, 402);
-    return (0);
+	va_list valist;
+	unsigned int i;
+	int sum = 0;
+
+	if (n == 0)
+		return (0);
+
+	va_start(valist, n);
+
+	for (i = 0; i < n; i++)
+		sum += va_arg(valist, int);
+
+	va_end(valist);
+
+	return (sum);
 }
